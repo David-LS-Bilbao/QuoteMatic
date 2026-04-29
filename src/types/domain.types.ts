36048eac46f1a47f052c8f3fb4cbebbd23610635
davidlopezsotelo <85489250+davidlopezsotelo@users.noreply.github.com>
2026-04-29 13:52:32 +0200

    feat(types): add quote type slugs

--- src/types/domain.types.ts
+++ src/types/domain.types.ts
@@ -1,25 +1,48 @@
 
-export const USER_ROLES = ["user", "admin"] as const;
+// roles de usuarios
+export const USER_ROLES = [
+    "user", 
+    "admin"
+] as const;
 export type UserRole = (typeof USER_ROLES)[number];
 
-export const AGE_GROUP = ["teen_14_17", "adult_18_plus"] as const;
-export type AgeGroup = (typeof AGE_GROUP)[number];
+// grupos de edad
+export const AGE_GROUPS = [
+    "teen_14_17", 
+    "adult_18_plus"
+] as const;
+export type AgeGroup = (typeof AGE_GROUPS)[number];
 
+
+// tipos de autores
 export const AUTHOR_TYPES = [
     "real",
   "historical",
   "fictional",
   "unknown",
 ] as const;
-
 export type AuthorType = (typeof AUTHOR_TYPES)[number];
 
-export const SOURCE_TYPES = ["book", "movie", "tv_show","historical","original", "unknown"] as const;
+// tipos de fuentes
+export const SOURCE_TYPES = [
+    "book", 
+    "movie",
+     "tv_show",
+     "historical",
+     "original", 
+     "unknown"
+    ] as const;
 export type SourceType = (typeof SOURCE_TYPES)[number];
 
-export const CONTENT_RATNGS = ["all","teen","adult"] as const;
-export type ContentRating = (typeof CONTENT_RATNGS)[number];
+// rangos de edades
+export const CONTENT_RATINGS = [
+    "all",
+    "teen",
+    "adult"
+] as const;
+export type ContentRating = (typeof CONTENT_RATINGS)[number];
 
+// estados de verificación
 export const VERIFICATION_STATUSES = [
   "original",
   "pending",
@@ -28,3 +51,17 @@ export const VERIFICATION_STATUSES = [
   "disputed",
 ] as const;
 export type VerificationStatus = (typeof VERIFICATION_STATUSES)[number];
+
+// tipos de frases disponibles en la app
+export const QUOTE_TYPE_SLUGS = [
+  "stoic",
+  "philosophical",
+  "motivational",
+  "funny",
+  "realistic",
+  "sarcastic",
+  "wise_advice",
+  "excuse",
+] as const;
+
+export type QuoteTypeSlug = (typeof QUOTE_TYPE_SLUGS)[number];
